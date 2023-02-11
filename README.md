1f9d95d08ba46bbbe8a8640532ee70bf70551de7
rkml14 <119961747+rkml14@users.noreply.github.com>
2023-02-11 13:30:48 -0500

    attempting function for shape choice

--- README.md
+++ README.md
@@ -21,20 +21,24 @@ SO THAT I don't have to pay a graphic designer
 
 ```md
 GIVEN a command-line application that accepts user input
-WHEN I am prompted for text
-THEN I can enter up to three characters
-WHEN I am prompted for the text color
-THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for text  CREATED THIS PROMPT 
+THEN I can enter up to three characters CREATED THIS PROMPT BUT WE NEED TO CHECK FOR ERROR
+WHEN I am prompted for the text color 
+THEN I can enter a color keyword (OR a hexadecimal number)  CREATED THIS   LIKELY NEED AN ERROR CHECK FOR NULL VALUE OR UNDEFINED
 WHEN I am prompted for a shape
-THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square  DONE 
 WHEN I am prompted for the shape's color
-THEN I can enter a color keyword (OR a hexadecimal number)
+THEN I can enter a color keyword (OR a hexadecimal number) CREATED THIS   LIKELY NEED AN ERROR CHECK FOR NULL VALUE OR UNDEFINED
 WHEN I have entered input for all the prompts
-THEN an SVG file is created named `logo.svg`
-AND the output text "Generated logo.svg" is printed in the command line
+THEN an SVG file is created named `logo.svg`  THIS WORKS BUT WE NEED TO REFINE FOR OTHER SHAPES
+AND the output text "Generated logo.svg" is printed in the command line THIS WORKS 
 WHEN I open the `logo.svg` file in a browser
 THEN I am shown a 300x200 pixel image that matches the criteria I entered
 ```
+NEXT: FUNCTION FOR TRIANGLE/SQUARE/CIRCLE  TO BE WRITTEN  
+
+NEXT: TESTING WRITEUPS
+
 
 ## Mock-Up
 
--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Circle = require('./lib/circle');
-const Square = require('./lib/Square');
-const Triangle = require('./lib/Triangle');
+const Square = require('./lib/square');
+const Triangle = require('./lib/triangle');
+
 
 const questions =
     [
         {
             type: 'input',
-            message: 'Please enter up to 3 characters for your logo',
+            message: 'Please enter up to 3 characters for your logo',   //going to need failure check here
             name: 'logo',
         },
         {
             type: 'input',
-            message: 'Please enter a color keyword or hexadecimal number for your TEXT colour:',
+            message: 'Please enter a color keyword or hexadecimal number for your TEXT colour:',  //may need failure check for colour
             name: 'textcolor',
         },
         {
@@ -24,27 +25,74 @@ const questions =
         },
         {
             type: 'input',
-            message: 'Please enter a color keyword or hexadecimal number for your SHAPE colour:',
+            message: 'Please enter a color keyword or hexadecimal number for your SHAPE colour:', //may need failure check for colour
             name: 'shapecolor',
         },
     ]
 
-    function writeToFile(fileName, data) {
-  
+function writeToFile(fileName, data) {
 
-        fs.writeFile(fileName, data, (err) => 
-        err ? console.log(err): console.log("Generated logo.svg"))
-    
+
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log("Generated logo.svg"))
+
+}
+
+function shapeChoice(shape) {
+    if (shape === circle) {
+        console.log(response);
+        const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
+        writeToFile('logo.svg', circle.render())
+    }
+    else {
+        if (shape === square) {
+            console.log(response);
+            const square = new Square(response.logo, response.textcolor, response.shapecolor);
+            writeToFile('logo.svg', square.render())
+        }
+        else {
+            (shape === triangle)
+            console.log(response);
+            const triangle = new Triangle(response.logo, response.textcolor, response.shapecolor);
+            writeToFile('logo.svg', triangle.render())
+        }
+    }
 }
 
 function init() {
     inquirer.prompt(questions)
-    .then(response =>{
-        console.log(response);
-        const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
-        // let readText = utils(response)
-        writeToFile('logo.svg', circle.render())
-    }) 
-}
+        .then((answers) => {
+        shapeChoice(shape);
+    })
+   }
+
+
+
 
 init();
+
+
+// function init() {
+//     inquirer.prompt(questions)
+//     .then(response =>{
+//         console.log(response);
+//         const circle = new Circle(response.logo, response.textcolor, response.shapecolor);
+//         writeToFile('logo.svg', circle.render())
+//     }) 
+// }
+
+
+
+// return writeFile(
+//     join(__dirname, '..', 'examples', 'logo.svg', circle.render()),
+//     // createDocument(this.title, this.tasks)  from mini project
+//   );
+
+
+
+// .then(() => console.log('Generated logo.svg'))
+// .catch((err) => {
+//   console.log(err);
+//   console.log('Oops. Something went wrong.');
+// });
+
